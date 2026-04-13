b058a721633c7212bcc621cfa3ba517ca2d7992c
anoukdevoss <anoukdevos.adv@gmail.com>
2026-04-13 13:28:52 +0200

    Initial commit

---
+++ src/main/java/nl/miwnn/cohort19/DeExparts/Overview/OverviewApplication.java
@@ -0,0 +1,13 @@
+package nl.miwnn.cohort19.DeExparts.Overview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OverviewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/nl/miwnn/cohort19/DeExparts/Overview/OverviewApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.miwnn.cohort19.DeExparts.Overview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OverviewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
