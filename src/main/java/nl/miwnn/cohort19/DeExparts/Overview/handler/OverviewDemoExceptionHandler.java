b4299ca1573d31acb120d5b76882d3d373ec1f8b
anoukdevoss <anoukdevos.adv@gmail.com>
2026-04-24 13:03:25 +0200

    Foutafhandling 404/500 toegevoegd

---
+++ src/main/java/nl/miwnn/cohort19/DeExparts/Overview/handler/OverviewDemoExceptionHandler.java
@@ -0,0 +1,31 @@
+package nl.miwnn.cohort19.DeExparts.Overview.handler;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.server.ResponseStatusException;
+
+/**
+ * Author: Anouk de Vos
+ * TODO description !!
+ */
+
+@ControllerAdvice
+public class OverviewDemoExceptionHandler {
+    @ExceptionHandler(ResponseStatusException.class)
+    public String handleNotFound(
+            ResponseStatusException exception,
+            Model model) {
+        model.addAttribute("statusCode",
+                exception.getStatusCode().value());
+        model.addAttribute("bericht", exception.getReason());
+        return "error/404";
+    }
+    @ExceptionHandler(Exception.class)
+    public String handleAlgemeneUitzondering(
+            Exception exception,
+            Model model) {
+        model.addAttribute("bericht", exception.getMessage());
+        return "error/500";
+    }
+}
